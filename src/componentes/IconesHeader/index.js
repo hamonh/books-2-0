b31b3d9158f79c625943ff0a4beb4e3a253bcc59
Hamon andrade <hamonfranca@gmail.com>
2025-05-15 10:59:33 -0300

    Barra de pesquisa
    useState/ Estilos da barra de pesquisa/ Dados/ Input

--- src/componentes/IconesHeader/index.js
+++ src/componentes/IconesHeader/index.js
@@ -1,16 +1,25 @@
-import './estilo.css'
 import perfil from '../../imagens/perfil.svg'
 import sacola from '../../imagens/sacola.svg'
+import styled from 'styled-components'
 
+const IconesContainer = styled.ul`
+    display: flex;
+    align-items: center;
+`
+const IconeContainer = styled.li`
+    margin-right: 40px;
+    width: 25px;
+    list-style: none;
+`
 const icones = [perfil, sacola]
 
 function IconesHeader() {
     return (
-        <ul className='icones'>
+        <IconesContainer>
             { icones.map( (icone) => (
-              <li className='icone'><img src={icone}></img></li>
+              <IconeContainer><img src={icone}></img></IconeContainer>
             )) }
-        </ul>
+        </IconesContainer>
     )
 }
 
--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,14 +1,28 @@
-import './estilo.css'
+import styled from 'styled-components'
 
+const OpcoesContainer = styled.ul`
+    display: flex;
+`
+const OpcaoContainer = styled.li`
+    font-size: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100%;
+    padding: 0 5px;
+    cursor: pointer;
+    min-width: 120px;
+`
 const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
 
 function OpcoesHeader() {
     return (
-        <ul className='opcoes'>
+        <OpcoesContainer>
             { textoOpcoes.map( (texto) => (
-                <li className='opcao'><p>{texto}</p></li>
+                <OpcaoContainer><p>{texto}</p></OpcaoContainer>
             ) ) }
-      </ul>
+      </OpcoesContainer>
     )
 }
 
